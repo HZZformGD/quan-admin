8ee4c9e9dd1c64302ccf140cd7d368348cea891e
czk <czk@example.com>
2019-02-19 10:23:08 +0800

    背景广告滚动页面

---
+++ src/models/app/scrollad.js
@@ -0,0 +1,42 @@
+import { appScrolladList,editScrollad } from '../../services/api';
+
+export default {
+  namespace: 'scrollad',
+
+  state: {
+    list: [],
+    total: 0,
+    domain: '',
+    arr: [],
+    checkList:[],
+    wxAppList:[],
+  },
+
+  effects: {
+    *getList({ payload }, { call, put }) {
+      const response = yield call(appScrolladList, payload);
+      const data = response.data;
+      const page = payload.page;
+      yield put({
+        type: 'queryList',
+        payload: data,
+      });
+    },
+
+    *editScrollad({ payload }, { call }) {
+      const response = yield call(editScrollad, payload);
+      return response;
+    },
+  },
+
+  reducers: {
+    queryList(state, action) {
+      return {
+        ...state,
+        list: action.payload.list,
+        total: Number(action.payload.total),
+        wxAppList:Object.values(action.payload.wxAppList),
+      };
+    },
+  },
+};
